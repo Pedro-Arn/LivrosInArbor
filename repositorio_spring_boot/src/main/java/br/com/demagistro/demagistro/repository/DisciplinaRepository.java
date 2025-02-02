e9493190a27115bb9a4c49d2a0165507393d6e1c
Pedro Arnaldo <pedroarnaldo09@gmail.com>
2025-02-02 14:05:29 -0300

    Retirada códigos springboot

--- repositorio_spring_boot/src/main/java/br/com/demagistro/demagistro/repository/DisciplinaRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.demagistro.demagistro.repository;
-
-import br.com.demagistro.demagistro.entity.Disciplina;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DisciplinaRepository extends JpaRepository<Disciplina, Integer> {
-    public Disciplina findByNome(String nome);
-}
